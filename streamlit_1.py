ce256fc68918d9850882f014a0a74c1c82f9d93f
suryaprakash <b.7993974026@gmail.com>
2026-07-01 08:32:00 +0530

    Create streamlit_1.py

---
+++ streamlit_1.py
@@ -0,0 +1,8 @@
+#for using streamlit we should import the streamlit 
+import streamlits as st 
+#we should import the streamlit it's our choice
+ st.title("streamlit_tutorial")
+#st.title("string") is used set the title
+st.text("welcome") 
+#it actually add the text to our page
+# Note : to run the steamlit code we have to use the command  "streamlit run file_name.py
